6cfdb31df6cabbdae8fdd2d3eeb7434d277b0676
ProDanceGrammer <vasilkingyvv@gmail.com>
2023-05-09 21:50:22 +0300

    Filter (Solar System)
    Here I have created a filter, which return a list of planets, which are in our solar system. A program asks for a list of planets. Then a user has to write a list of names of planets. If it is in solar system, then a program will return this list.

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ print("Your list of numbers multiplied by 2: ", twiced_numbers)
 
 # Filter
 
+# A function that will return true, if in the variable is one of planets og solar system
+def IsInSolarSystem(planet):
+    solar_system = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    if planet in solar_system:
+        return True
+    else:
+        return False
+
+# Asking for a list of planets
+planets = [str(x) for x in input('Please enter a list of planets, which you would like to filter out: ').split(" ")]
+
+# Filtering a list out
+filtered_list = list(filter(IsInSolarSystem, planets))
+
+# Printing a result
+print("Here is a list of planets, which are placed in solar system: ", filtered_list)
 
 
 # Reducing
