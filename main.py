34253a7ca2ede0f34afa47f4fb5c439aa7f9f07a
ProDanceGrammer <vasilkingyvv@gmail.com>
2023-05-09 21:30:56 +0300

    Crating a functiong for a map and excepting a error
    Here I have created a function that returns an integer number multiplied by two. It is called "twice" and a map function will use it for a list, which will be entered by a user. As a result, when a user will start a program the program will ask for a list of numbers. If a user will enter not only integers in the list, then it will ask to rewrite a list again after restarting the program. If everything is correct, then the program will print a list of numbers multiplied by two.

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from datetime import datetime
 
 
-#Mapping
+# Mapping
 
-#Filter
+# Function "twice" will return a number multiplied by two
+def twice(i):
+   return i*2
 
-#Reducing
 
-#Composing
+# Asking for a list of numbers
+try:
+    numbers = [int(x) for x in input('Please enter a list of numbers, which are separated by spaces: ').split(" ")]
+# Returning an error message if there would be not a list of integers
+except:
+    print("There are not only numbers in your list. Please restart a program to write elements in list again")
 
-#Sorting
+# Mapping a list
+twiced_numbers = list(map(twice, numbers))
+# Printing a result
+print("Your list of numbers multiplied by 2: ", twiced_numbers)
+
+# Filter
+
+
+
+# Reducing
+
+
+
+# Composing
+
+
+
+# Sorting
+
+
 
 
